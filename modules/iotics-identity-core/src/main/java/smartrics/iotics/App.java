04aa4a6a980035e540516f8c4b72c74a15dbd2b3
Fabrizio Cannizzo <smartrics@gmail.com>
2024-04-16 14:09:28 +0100

    initial commit after modules creation

---
+++ modules/iotics-identity-core/src/main/java/smartrics/iotics/App.java
@@ -0,0 +1,13 @@
+package smartrics.iotics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
