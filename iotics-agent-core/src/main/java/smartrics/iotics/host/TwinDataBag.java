a285e39f5b94fa9ddb181bc54ef380117b802674
Fabrizio Cannizzo <smartrics@gmail.com>
2024-04-17 09:30:24 +0100

    fixed broken build due to missing provided dependency

--- iotics-agent-core/src/main/java/smartrics/iotics/host/TwinDataBag.java
+++ iotics-agent-core/src/main/java/smartrics/iotics/host/TwinDataBag.java
@@ -3,40 +3,16 @@ package smartrics.iotics.host;
 import com.iotics.api.*;
 
 import java.util.List;
-import java.util.Optional;
 
-public record   TwinDataBag(List<Property> properties, TwinID twinID, int feedsCount, int inputsCount,
+public record TwinDataBag(List<Property> properties, TwinID twinID, int feedsCount, int inputsCount,
                           GeoLocation location) {
 
     public static TwinDataBag from(SearchResponse.TwinDetails twinDetails) {
-        return new TwinDataBag(
-                twinDetails.getPropertiesList(),
-                twinDetails.getTwinId(),
-                twinDetails.getFeedsCount(),
-                twinDetails.getInputsCount(),
-                twinDetails.getLocation());
+        return new TwinDataBag(twinDetails.getPropertiesList(), twinDetails.getTwinId(), twinDetails.getFeedsCount(), twinDetails.getInputsCount(), twinDetails.getLocation());
     }
 
     public static TwinDataBag from(DescribeTwinResponse twinDescription) {
-        return new TwinDataBag(
-                twinDescription.getPayload().getResult().getPropertiesList(),
-                twinDescription.getPayload().getTwinId(),
-                twinDescription.getPayload().getResult().getFeedsCount(),
-                twinDescription.getPayload().getResult().getInputsCount(),
-                twinDescription.getPayload().getResult().getLocation());
-    }
-
-    public Optional<TwinID> optionalModelTwinID() {
-        return this.optionalModelTwinID(twinID.getHostId());
-    }
-
-    public Optional<TwinID> optionalModelTwinID(String hostID) {
-        List<String> list = properties.stream()
-                .filter(property -> property.getKey().equals(UriConstants.IOTICS_APP_MODEL_PROP)).map(p -> p.getUriValue().getValue()).toList();
-        if (!list.isEmpty()) {
-            return Optional.of(TwinID.newBuilder().setId(list.get(0)).setHostId(hostID).build());
-        }
-        return Optional.empty();
+        return new TwinDataBag(twinDescription.getPayload().getResult().getPropertiesList(), twinDescription.getPayload().getTwinId(), twinDescription.getPayload().getResult().getFeedsCount(), twinDescription.getPayload().getResult().getInputsCount(), twinDescription.getPayload().getResult().getLocation());
     }
 
 }
